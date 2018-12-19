abd7ba4893ed91c2fc112fb93b021b769aa48e63
Ranae Rodgers <ranae.rodgers@icloud.com>
2018-12-19 01:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const two = 2;
 function add2(n) {
   return (n+2);
 }
-var funkyFunction = function()
-function funkyFunction() {
-}
+var funkyFunction = function();
+return function();
+return "FUNKY!"
